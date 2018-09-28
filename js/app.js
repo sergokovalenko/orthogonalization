b2f2087de791ac813316aecec5d790d966fc9411
Sergey Kovalenko <kovalenkosergey.98@yandex.ru>
2018-09-28 21:17:04 +0400

    files structure

---
+++ js/app.js
@@ -0,0 +1,6 @@
+import * as calc from './ortogonalization.js';
+
+window.onload = () => {
+  const startValue = document.getElementById('matrix');
+  const resultBlock = document.getElementById('result');
+};
