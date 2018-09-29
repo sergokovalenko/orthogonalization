825976d2d11043c7efcc2106979b0dbc77695d6d
Sergey Kovalenko <kovalenkosergey.98@yandex.ru>
2018-09-29 12:09:27 +0400

    transposition

--- js/app.js
+++ js/app.js
@@ -8,7 +8,12 @@ window.onload = () => {
     [0.12, -0.11, 0, 0.49, 0.112, -1.464],
     [-0.05, 0, 0.025, -0.01, 0.294, 2.362],
   ];
+  const example = [
+    [0.4, 0.3, -0.2, 2],
+    [0.6, -0.5, 0.3, 2.5],
+    [0.3, 0.2, 0.5, 11],
+  ];
   const startValue = document.getElementById('matrix');
   const resultBlock = document.getElementById('result');
-  calc.calc(matrix);
+  calc.calc(example);
 };
--- js/ortogonalization.js
+++ js/ortogonalization.js
@@ -15,10 +15,21 @@ export function calc(matrix) {
 	}
 
 	let j12 = calculateLambda(a[1], r[0]);
-	r[1].push(sumVectors(a[1], multiplicateNumberAndVector(j12, r[0])));
+	console.log(j12);
+	r[1] = sumVectors(a[1], multiplicateNumberAndVector(-1 * j12, r[0]));
+	console.log(r[1]);
 
 	let j13 = calculateLambda(a[2], r[0]);
 	let j23 = calculateLambda(a[2], r[1]);
+	console.log(j13);
+	console.log(j23);
+
+	let tmp1 = multiplicateNumberAndVector(-1 * j13, r[0]);
+	let tmp2 = multiplicateNumberAndVector(-1 * j23, r[1]);
+
+	r[2] = sumLotsVectors(a[2], tmp1, tmp2);
+
+	console.log(r[2]);
 }
 
 function calculateLambda(a, r) {
@@ -47,4 +58,25 @@ function sumVectors(v1, v2) {
 	}, 0);
 
 	return result;
+}
+
+export function sumLotsVectors(...vectors) {
+	let result = [0, 0, 0];
+	console.log(vectors.length);
+	for (let i = 0; i < vectors.length; i++) {
+		for (let j = 0; j < vectors[i].length; j++) {
+			result[j] += vectors[i][j];
+		}
+	}
+
+	return result;
+}
+
+function TransMatrix(matrix) {
+	let m = matrix.length, n = matrix[0].length, AT = [];
+	for (let i = 0; i < n; i++) {
+	AT[i] = [];
+		for (let j = 0; j < m; j++) AT[i][j] = matrix[j][i];
+	}
+	return AT;
 }
