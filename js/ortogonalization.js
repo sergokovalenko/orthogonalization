97c0b8694c09a57d267cb947edfd455ea8dfe4e5
Sergey Kovalenko <kovalenkosergey.98@yandex.ru>
2018-09-29 12:13:42 +0400

    determinant

--- js/ortogonalization.js
+++ js/ortogonalization.js
@@ -72,11 +72,60 @@ export function sumLotsVectors(...vectors) {
 	return result;
 }
 
-function TransMatrix(matrix) {
+function transMatrix(matrix) {
 	let m = matrix.length, n = matrix[0].length, AT = [];
 	for (let i = 0; i < n; i++) {
-	AT[i] = [];
+		AT[i] = [];
 		for (let j = 0; j < m; j++) AT[i][j] = matrix[j][i];
 	}
 	return AT;
+}
+
+function determinant(A) {
+	let N = A.length, B = [], denom = 1, exchanges = 0;
+
+	for (let i = 0; i < N; ++i) {
+		B[i] = [];
+		for (let j = 0; j < N; ++j) {
+			B[i][j] = A[i][j]
+		};
+	}
+
+	for (let i = 0; i < N - 1; ++i) {
+		let maxN = i, maxValue = Math.abs(B[i][i]);
+
+		for (let j = i + 1; j < N; ++j) {
+			let value = Math.abs(B[j][i]);
+			if (value > maxValue) {
+				maxN = j; maxValue = value;
+			}
+		}
+
+		if (maxN > i) {
+			let temp = B[i]; B[i] = B[maxN]; B[maxN] = temp;
+			exchanges += 1;
+		} else {
+			if (maxValue == 0) {
+				return maxValue;
+			}
+		}
+
+		let value1 = B[i][i];
+
+		for (let j = i + 1; j < N; ++j) {
+			let value2 = B[j][i];
+			B[j][i] = 0;
+
+			for (let k = i + 1; k < N; ++k) {
+				B[j][k] = (B[j][k] * value1 - B[i][k] * value2) / denom;
+			}
+		}
+		denom = value1;
+	}
+
+	if (exchanges % 2) {
+		return -B[N - 1][N - 1];
+	}
+
+	return B[N - 1][N - 1];
 }
